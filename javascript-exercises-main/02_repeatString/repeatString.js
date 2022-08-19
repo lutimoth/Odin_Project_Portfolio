6e53d3040732dd66bedd16ac0b04f5da710fe085
naturesbless <lutimoth@usc.edu>
2022-08-18 22:50:16 -0700

    Finish repeatString and reverseString

--- javascript-exercises-main/02_repeatString/repeatString.js
+++ javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    repeat = '';
+    if (num < 0){
+        return 'ERROR'
+    }
+    for (let i = 0 ; i < num ; i++){
+        repeat += string;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
--- javascript-exercises-main/03_reverseString/reverseString.js
+++ javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const split = string.split("");
+    new_str = ""
+    for (let i = split.length - 1; i >= 0; i--){
+        new_str += split[i]
+    }
+    return new_str
 };
 
 // Do not edit below this line
