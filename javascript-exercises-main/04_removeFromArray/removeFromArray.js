0a3740e8f95c8e98990a4ea57534c8d2f6585182
naturesbless <lutimoth@usc.edu>
2022-08-18 23:49:27 -0700

    Finishing practrice questions

--- javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {    
+    const arr = arguments[0];
+    const newArray = [];
 
+    arr.forEach((item) => {
+        if (!args.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
--- javascript-exercises-main/05_sumAll/sumAll.js
+++ javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 | num2 < 0){
+        return 'ERROR'
+    }
 
+    if (typeof num1 != 'number' | typeof num2 != 'number'){
+        return 'ERROR'
+    }
+
+    if (num1 < num2){
+        smaller = num1;
+        bigger = num2;
+    } else {
+        smaller = num2;
+        bigger = num1;
+    }
+    
+    let distance = bigger - smaller
+    let i = 1;
+    let total = smaller
+
+    while (i <= distance){
+        total += smaller + i
+        i++
+    }
+    return total
 };
 
 // Do not edit below this line
